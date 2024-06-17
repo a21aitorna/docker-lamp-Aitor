c727718694b77f977b218deb362ba2e1d965ad43
Nombre de alumno <nombre@alumno>
2024-06-17 20:48:27 +0200

    Ejercicio 1 completo

---
+++ www/ParcialT1Examen/e1.php
@@ -0,0 +1,31 @@
+<?php
+
+    function contarVocales($stringCualquiera){
+        $vocales=0;
+        $stringCualquiera=strtolower($stringCualquiera);
+        for($i=0;$i<=strlen($stringCualquiera);$i++){
+            // if($stringCualquiera[$i]=='a'){//||$stringCualquiera[$i]='e'||$stringCualquiera[$i]='i'||$stringCualquiera[$i]='o'||$stringCualquiera[$i]='u'||$stringCualquiera[$i]='A'||$stringCualquiera[$i]='E'||$stringCualquiera[$i]='I'||$stringCualquiera[$i]='O'||$stringCualquiera[$i]='U'){
+            if(in_array($stringCualquiera[$i],['a','e','i','o','u'])){
+                $vocales++;
+            } 
+        }
+        return $vocales;
+    }
+
+    function obtenerVocales($stringCualquiera){
+        $vocalesPresentes = array();
+        $vocalesDisponibles = array('a','e','i','o','u','A','E','I','O','U');
+        for($i=0;$i<strlen($stringCualquiera);$i++){
+            if(in_array($stringCualquiera[$i],$vocalesDisponibles)){
+                foreach($vocalesDisponibles as $vocal){
+                    if($stringCualquiera[$i]==$vocal){
+                        array_push($vocalesPresentes, $vocal);
+                    }
+                }
+                
+            }
+        }
+        return $vocalesPresentes;
+
+    }
+?>
---
+++ www/ParcialT1Examen/e1_test.php
@@ -0,0 +1,10 @@
+<?php
+    
+require 'e1.php';
+
+$stringCualquiera='Esto es un string cualquiera';
+
+echo ("Se han encontrado ".contarVocales($stringCualquiera)." vocales<br>=================<br>");
+var_dump(obtenerVocales($stringCualquiera));
+
+?>
